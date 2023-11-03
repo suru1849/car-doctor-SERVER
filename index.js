663e4f7cde7c27c96cc96f88cf32d105b38b0bfb
suru1849 <suranjan1849@gmail.com>
2023-11-04 02:35:41 +0600

    last push

--- index.js
+++ index.js
@@ -17,6 +17,28 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+const logger = async (req, res, next) => {
+  console.log("Log info: ", req.method, req.url, req.host, req.originalUrl);
+
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log("Token from middleWare ", token);
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorised Acccess" });
+  }
+  jwt.verify(token, process.env.ACCESS_SECRETE_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorised Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kapryhp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,28 +61,38 @@ async function run() {
     const bookingsCollection = dataBase.collection("bookings");
 
     //  Auth related API
-    app.post("/jwt", async (req, res) => {
+    app.post("/jwt", logger, async (req, res) => {
       const user = req.body;
-      console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
+      console.log("Token requested user", user);
+      // Generating token
+      const token = jwt.sign(user, process.env.ACCESS_SECRETE_TOKEN, {
         expiresIn: "1h",
       });
+
       res
         .cookie("token", token, {
           httpOnly: true,
-          secure: false,
+          secure: true,
+          sameSite: "none",
         })
         .send({ success: true });
     });
 
+    app.post("/logout", logger, async (req, res) => {
+      const user = req.body;
+      console.log("Logging out user: ", user);
+
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
     // services related API
-    app.get("/services", async (req, res) => {
+    app.get("/services", logger, async (req, res) => {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get("/services/:id", async (req, res) => {
+    app.get("/services/:id", logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
@@ -76,17 +108,22 @@ async function run() {
     });
 
     // bookings
-    app.post("/bookings", async (req, res) => {
+    app.post("/bookings", logger, async (req, res) => {
       const booking = req.body;
       console.log(booking);
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
 
-    app.get("/bookings", async (req, res) => {
-      let query = {};
-      console.log("TOKEN: ", req.cookies.token);
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      // console.log("cookies", req.cookies);
+      console.log(req.query?.email);
+      console.log("Token owner info: ", req?.user);
+      if (req.query?.email !== req?.user?.email) {
+        return res.status(403).send({ message: "Forbbider Access" });
+      }
 
+      let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
       }
@@ -94,14 +131,14 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/bookings/:id", async (req, res) => {
+    app.delete("/bookings/:id", logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await bookingsCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.patch("/bookings/:id", async (req, res) => {
+    app.patch("/bookings/:id", logger, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const updatedDoc = {
